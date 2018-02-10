b366344d82fcbc08bc40a71bdea06078802d7b8d
Dibbo <DeflatedPickle@gmail.com>
2018-02-10 12:11:51 +0000

    Fixed #1

--- src/main/kotlin/org/koilang/sukoi/Util.kt
+++ src/main/kotlin/org/koilang/sukoi/Util.kt
@@ -6,7 +6,11 @@ fun koiPrint(valueList: List<KoiParser.ValueContext>): String {
     var printValue = ""
 
     for (item in valueList) {
+        if (isSingleString(item.text) || isLitString(item.text) || isMultiString(item.text)) {
             printValue += item.text.substring(1, item.text.length - 1)
+        } else {
+            printValue += item.text
+        }
 
         if (item != valueList[valueList.size - 1]) {
             printValue += " "
@@ -14,4 +18,28 @@ fun koiPrint(valueList: List<KoiParser.ValueContext>): String {
     }
 
     return printValue
+}
+
+fun isSingleString(value: String): Boolean {
+    if (value.startsWith("\"") && value.endsWith("\"")) {
+        return true
+    }
+
+    return false
+}
+
+fun isLitString(value: String): Boolean {
+    if (value.startsWith("'") && value.endsWith("'")) {
+        return true
+    }
+
+    return false
+}
+
+fun isMultiString(value: String): Boolean {
+    if (value.startsWith("`") && value.endsWith("`")) {
+        return true
+    }
+
+    return false
 }

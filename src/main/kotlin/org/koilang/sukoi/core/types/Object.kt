9cfde7ce47cfaa5affdf92404c33c0e34f026254
DeflatedPickle <DeflatedPickle@gmail.com>
2018-03-27 22:06:35 +0100

    Added an equals method

--- src/main/kotlin/org/koilang/sukoi/core/types/Object.kt
+++ src/main/kotlin/org/koilang/sukoi/core/types/Object.kt
@@ -2,4 +2,13 @@ package org.koilang.sukoi.core.types
 
 open class Object {
     fun new() {}
+
+    fun equals(other: Object): Boolean {
+        if (this == other) {
+            return Boolean(true)
+        }
+        else {
+            return Boolean(false)
+        }
+    }
 }

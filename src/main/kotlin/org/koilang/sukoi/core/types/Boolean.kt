fb6e6cce7d932d554fcd9d83482e48c90515eb22
Dibbo <DeflatedPickle@gmail.com>
2018-03-28 12:25:22 +0100

    Added a not method

--- src/main/kotlin/org/koilang/sukoi/core/types/Boolean.kt
+++ src/main/kotlin/org/koilang/sukoi/core/types/Boolean.kt
@@ -1,6 +1,10 @@
 package org.koilang.sukoi.core.types
 
 class Boolean(private var value: kotlin.Boolean) : Value() {
+    fun not(): Boolean {
+        return Boolean(!value)
+    }
+
     override fun toBoolean(): Boolean {
         return Boolean(value)
     }

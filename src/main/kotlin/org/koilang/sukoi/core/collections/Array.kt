82e397e18b25fe8e829822c17bc0f7b244a6f6f1
Dibbo <DeflatedPickle@gmail.com>
2018-03-28 15:50:19 +0100

    Added a length method

--- src/main/kotlin/org/koilang/sukoi/core/collections/Array.kt
+++ src/main/kotlin/org/koilang/sukoi/core/collections/Array.kt
@@ -1,10 +1,15 @@
 package org.koilang.sukoi.core.collections
 
+import org.koilang.sukoi.core.types.Integer
 import org.koilang.sukoi.core.types.Object
 import org.koilang.sukoi.core.types.Reference
 
 abstract class Array<T>(private var value: ArrayList<out Object>) : Reference() {
     fun append(other: Object) {
         value + other
+    }
+
+    fun length(): Integer {
+        return Integer(value.size)
     }
 }
